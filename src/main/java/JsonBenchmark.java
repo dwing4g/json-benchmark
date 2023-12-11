309a619f08328a833ef6b012e379cab39d6d9b25
dwing <dwing4g@gmail.com>
2023-12-11 17:25:11 +0800

    add NumberBenchmark; update gradle and dependencies; update to JDK21

--- src/main/java/JsonBenchmark.java
+++ src/main/java/JsonBenchmark.java
@@ -1,14 +1,16 @@
 import com.alibaba.fastjson2.JSON;
+import com.dslplatform.json.CompiledJson;
 import com.dslplatform.json.DslJson;
 import com.dslplatform.json.runtime.Settings;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.jsoniter.JsonIterator;
 import com.jsoniter.spi.DecodingMode;
-import jason.JasonReader;
+import jason.JsonReader;
 
 // --add-opens java.base/java.lang=ALL-UNNAMED
 public class JsonBenchmark {
+	@CompiledJson
 	public static class C { // 测试的类
 		public int a;
 		@SuppressWarnings("unused")
@@ -21,7 +23,7 @@ public class JsonBenchmark {
 	static byte[] bytes = str.getBytes();
 	static final int TEST_COUNT = 10_000_000;
 
-	static void testJackson() throws Exception { // jackson-*-2.13.2.jar四个文件总大小2.09MB
+	static void testJackson() throws Exception { // jackson-*-2.16.0.jar四个文件总大小2.39MB
 		long t = System.nanoTime(), v = 0;
 		ObjectMapper om = new ObjectMapper();
 		om.registerModule(new AfterburnerModule()); // 实测这里Afterburner插件效果不理想,只提速15%
@@ -32,7 +34,7 @@ public class JsonBenchmark {
 		System.out.println(" Jackson: " + v + ", " + (System.nanoTime() - t) / 1_000_000 + "ms");
 	}
 
-	static void testFastJson() { // fastjson2-2.0.1.jar大小1.05MB
+	static void testFastJson() { // fastjson2-2.0.43.jar大小1.9MB
 		long t = System.nanoTime(), v = 0;
 		for (int i = 0; i < TEST_COUNT; ++i) {
 			// C c = JSON.parseObject(bytes, C.class);
@@ -42,7 +44,7 @@ public class JsonBenchmark {
 		System.out.println("FastJson: " + v + ", " + (System.nanoTime() - t) / 1_000_000 + "ms");
 	}
 
-	static void testDslJson() throws Exception { // dsl-json-1.9.9.jar和dsl-json-java8-1.9.9.jar总大小492KB
+	static void testDslJson() throws Exception { // dsl-json-2.0.2.jar大小447KB
 		long t = System.nanoTime(), v = 0;
 		DslJson<Object> dj = new DslJson<>(Settings.withRuntime().includeServiceLoader());
 		for (int i = 0; i < TEST_COUNT; ++i) {
@@ -53,9 +55,9 @@ public class JsonBenchmark {
 		System.out.println("Dsl-Json: " + v + ", " + (System.nanoTime() - t) / 1_000_000 + "ms");
 	}
 
-	static void testJsoniter() { // jsoniter-0.9.23.jar和javassist-3.28.0-GA.jar总大小1.14MB
+	static void testJsoniter() { // jsoniter-0.9.23.jar和javassist-3.29.2-GA.jar总大小1.09MB
 		long t = System.nanoTime(), v = 0;
-		// 设置最快的模式,需要借助javassist库(javassist-3.26.0-GA.jar大小765KB)
+		// 设置最快的模式,需要借助javassist库(javassist-3.29.2-GA.jar大小776KB)
 		JsonIterator.setMode(DecodingMode.DYNAMIC_MODE_AND_MATCH_FIELD_WITH_HASH);
 		for (int i = 0; i < TEST_COUNT; ++i) {
 			C c = JsonIterator.deserialize(bytes, C.class);
@@ -64,9 +66,9 @@ public class JsonBenchmark {
 		System.out.println("Jsoniter: " + v + ", " + (System.nanoTime() - t) / 1_000_000 + "ms");
 	}
 
-	static void testJason() throws Exception { // jason.jar大小45KB
+	static void testJason() throws Exception { // jason.jar大小49KB
 		long t = System.nanoTime(), v = 0;
-		JasonReader jr = new JasonReader();
+		JsonReader jr = new JsonReader();
 		for (int i = 0; i < TEST_COUNT; ++i) {
 			C c = jr.buf(bytes).parse(C.class);
 			//noinspection ConstantConditions
@@ -79,19 +81,23 @@ public class JsonBenchmark {
 		// 各测试5轮,排除预热影响,以最少时间为准
 		for (int i = 0; i < 5; ++i)
 			testJackson();
+		System.gc();
 		for (int i = 0; i < 5; ++i)
 			testFastJson();
+		System.gc();
 		for (int i = 0; i < 5; ++i)
 			testDslJson();
+		System.gc();
 		for (int i = 0; i < 5; ++i)
 			testJsoniter();
+		System.gc();
 		for (int i = 0; i < 5; ++i)
 			testJason();
 	}
 }
-// OpenJDK 17.0.2
-//  Jackson: 5790000000, 3606ms
-// FastJson: 5790000000, 1208ms
-// Dsl-Json: 5790000000, 2243ms
-// Jsoniter: 5790000000, 1058ms
-//    Jason: 5790000000, 871ms
+// OpenJDK 21.0.1
+//  Jackson: 5790000000, 4073ms
+// FastJson: 5790000000, 1154ms
+// Dsl-Json: 5790000000, 2446ms
+// Jsoniter: 5790000000, 1167ms
+//    Jason: 5790000000, 973ms
